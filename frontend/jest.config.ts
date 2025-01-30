c03a1e0dab002ec8f1b01d17311fd2625c9dd387
Henry Ventura <hvent90@gmail.com>
2025-01-30 12:05:01 -0800

    chore: Add license to files

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,4 +1,21 @@
 /**
+ * Copyright 2024 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+/**
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
@@ -158,7 +175,11 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: {
+    'visual': 'puppeteer',
+    'unit': 'jsdom'
+    // @ts-ignore
+  }[process.env.TEST_TYPE] ?? 'jsdom',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
